92bda33392fff19920761b859ba3d41f37349cf7
Stefan Hartmann <stefanhartmann@gmail.com>
2016-12-20 19:55:23 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const request = require('request');
+const nodemailer = require('nodemailer');
+
+app.use("/", express.static(__dirname + '/build'));
+var port = process.env.PORT || 3000;
+
+const transporter = nodemailer.createTransport('smtps://bearyourhartmann%40gmail.com:nicenicenicenice@smtp.gmail.com')
+
+const mailOptions = {
+  from: '"Your Loyal Server" <front@bearyourhartmann.com>',
+  // to: 'bearyourhartmann@gmail.com, stefanhartmann@gmail.com, thejessbear@gmail.com',
+  to: 'bearyourhartmann@gmail.com',
+  subject: 'Bus Form Update',
+  text: "Hello world",
+  html: '<h1>Hello World!</h1>'
+};
+
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+  console.log('this is the port: ', port);
+  console.log('mail server now online!');
+});
+
+app.post("/formupload", function(req, res){
+  console.log("this is the request body:", req.body)
+
+  mailOptions.text = req.body.textarea;
+  mailOptions.html = req.body.textarea;
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error){
+      return console.log("this is the error:", error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+  res.send("message sent")
+})
